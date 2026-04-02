7d2029b9ba402e12aa8d287891c6304290c20301
Guilherme Poppi <guilhermepoppilm@gmail.com>
2026-04-02 01:45:43 -0300

    fix: viewport

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,23 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Viewport-Width',
+            value: 'device-width',
+          },
+          {
+            key: 'X-UA-Compatible',
+            value: 'IE=edge',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
